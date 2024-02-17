9d44bd17f90bf79d50b0e872c3326fe1a7477e5f
Divyesh Canopas <divyesh.v@canopas.com>
2024-02-17 11:48:40 +0530

    designed workout detail screen

--- lib/UI/Home/WorkoutDetailView.dart
+++ lib/UI/Home/WorkoutDetailView.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_practical_2/Data/Repositories/UserRepository.dart';
+import 'package:flutter_practical_2/Styles/Spacer.dart';
+import 'package:flutter_practical_2/UI/Home/HomeView.dart';
 
 import '../../Data/Models/User.dart';
 import '../../Styles/AppTextStyle.dart';
@@ -15,6 +16,13 @@ class WorkoutDetailView extends StatefulWidget {
 class _WorkoutDetailViewState extends State<WorkoutDetailView> {
   final UserRepository _userRepository = UserRepository.shared;
   User? _user;
+  List<Color> colors = [
+    const Color(0xFFFFF3DE),
+    const Color(0xEBF2F4FF),
+    const Color(0xFBF2EBFF),
+    const Color(0xEBF2F4FF),
+    const Color(0xfffaf2e6)
+  ];
 
   @override
   void initState() {
@@ -33,9 +41,7 @@ class _WorkoutDetailViewState extends State<WorkoutDetailView> {
 
   @override
   Widget build(BuildContext context) {
-    return _user != null
-        ? _body(_user!)
-        : const CircularProgressIndicator();
+    return _user != null ? _body(_user!) : const CircularProgressIndicator();
   }
 
   Widget _body(User user) {
@@ -44,13 +50,15 @@ class _WorkoutDetailViewState extends State<WorkoutDetailView> {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            roundedCornerIconButton(icon: Icons.chevron_left),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('', style: AppTextStyle.subtitle(fontSize: 16)),
-                Text('', style: AppTextStyle.header1()),
+                Text('Sharon Howard', style: AppTextStyle.header1()),
+                Text('Yoga trainer', style: AppTextStyle.subtitle()),
               ],
             ),
+            roundedCornerIconButton(icon: Icons.more_horiz)
           ],
         ),
       ),
@@ -58,7 +66,132 @@ class _WorkoutDetailViewState extends State<WorkoutDetailView> {
         padding: const EdgeInsets.all(15),
         color: const Color(0xfffffcf9),
         child: ListView(
-          children: [],
+          children: [
+            roundedImage(
+                imageUrl: "assets/images/yoga_trainer.jpeg", radius: 100),
+            Vspacer(height: 15),
+            _workoutGrid(user.bodyMonitors ?? [], colors),
+            Vspacer(height: 15),
+            const DurationCard(),
+            Vspacer(height: 15),
+            getMoreWorkoutButton()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget roundedCornerIconButton(
+      {required IconData icon, Color color = Colors.black}) {
+    return IconButton(
+        onPressed: () {},
+        icon: Container(
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color(0xfffff3df)),
+          child: Icon(icon, color: color),
+        ));
+  }
+
+  Widget getMoreWorkoutButton() {
+    return TextButton(
+        onPressed: () {},
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xfffbce84),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          width: double.maxFinite,
+          height: 50,
+          child: Center(
+              child: Text("20 More workout",
+                  style: AppTextStyle.title(fontColor: Colors.white))),
+        ));
+  }
+}
+
+Widget _workoutGrid(List<BodyMonitors> monitoring, List<Color> colors) {
+  return Padding(
+    padding: const EdgeInsets.all(1.0),
+    child: GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16.0,
+        mainAxisSpacing: 16.0,
+      ),
+      itemCount: monitoring.length,
+      itemBuilder: (context, index) {
+        return bodyMonitorCell(monitoring[index], index, colors,
+            width: double.maxFinite, leftPadding: 0);
+      },
+    ),
+  );
+}
+
+class DurationCard extends StatefulWidget {
+  const DurationCard({super.key});
+
+  @override
+  State<DurationCard> createState() => _DurationCardState();
+}
+
+class _DurationCardState extends State<DurationCard> {
+  List<String> durations = ["1-10", "10-20", "20-30"];
+  String selectedDuration = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    selectedDuration = durations.first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color(0xffede5ff),
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Duration",
+              style: AppTextStyle.header2(),
+            ),
+            Vspacer(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ...durations.map((e) => Card(
+                      child: GestureDetector(
+                        onTap: () => setState(() {
+                          selectedDuration = e;
+                        }),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: e == selectedDuration
+                                  ? const Color(0xff9479d4)
+                                  : const Color(0xffede5ff),
+                              borderRadius: BorderRadius.circular(12)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text("$e min",
+                                style: AppTextStyle.medium(
+                                    fontColor: e == selectedDuration
+                                        ? Colors.white
+                                        : Colors.black)),
+                          ),
+                        ),
+                      ),
+                    )),
+              ],
+            ),
+          ],
         ),
       ),
     );
